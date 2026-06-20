93955f7d0f7f49bec608159fc338f882dbbf489c
Divy-unx <divyanshum635@gmail.com>
2026-06-20 20:00:00 +0530

    Solved ToLowerCase DSA problem.

---
+++ src/String/ToLowerCase.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.Scanner;
+
+public class ToLowerCase {
+    public static String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for(int i = 0; i < sb.length(); i++){
+            char ch = sb.charAt(i);
+
+            if(ch >= 'A' && ch <= 'Z'){
+                char lch =(char)('a' + ch - 'A');
+                sb.setCharAt(i,lch);
+            }
+        }
+        return sb.toString();
+    }
+
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        System.out.println(toLowerCase(string));
+    }
+}
+
