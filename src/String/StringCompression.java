f837e9ed0c6bd16c29941cd930c5e93d4ff62555
Divy-unx <divyanshum635@gmail.com>
2026-07-11 20:00:00 +0530

    Solved StringCompression problem.

--- src/String/StringCompression.java
+++ src/String/StringCompression.java
@@ -3,20 +3,6 @@ import java.lang.String;
 import java.util.Scanner;
 
 public class StringCompression {
-    public static String Compression1(String str){
-        StringBuilder s = new StringBuilder(str.charAt(0) + "");
-
-        for(int i = 1 ; i < str.length(); i++){
-            char curr = str.charAt(i);
-            char prev = str.charAt(i-1);
-
-            if(curr != prev){
-                s.append(curr);
-            }
-        }
-        return s.toString();
-    }
-
     public static String Compression2(String str){
         StringBuilder s = new StringBuilder(str.charAt(0) + "");
         int count = 1;
@@ -46,7 +32,6 @@ public class StringCompression {
     static void main() {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        System.out.println(Compression1(str));
         System.out.println(Compression2(str));
     }
 }
--- src/String/StringCompression1.java
+++ src/String/StringCompression1.java
@@ -1,4 +1,24 @@
 package String;
 
+import java.util.Scanner;
+
 public class StringCompression1 {
+    public static String Compression1(String str){
+        StringBuilder s = new StringBuilder(str.charAt(0) + "");
+
+        for(int i = 1 ; i < str.length(); i++){
+            char curr = str.charAt(i);
+            char prev = str.charAt(i-1);
+
+            if(curr != prev){
+                s.append(curr);
+            }
+        }
+        return s.toString();
+    }
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(Compression1(str));
+    }
 }
