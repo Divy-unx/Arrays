33e7f8a3a1bfcabe26669ee040b0e3e9efc95e59
Divy-unx <divyanshum635@gmail.com>
2026-07-11 20:00:00 +0530

    Solved MaximumLengthSubstringWithTwoOccurrences DSA problem.

---
+++ src/HashMap/MaximumLengthSubstringWithTwoOccurrences.java
@@ -0,0 +1,36 @@
+package HashMap;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MaximumLengthSubstringWithTwoOccurrences {
+    public static int maximumLengthSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxlen = 0;
+        int left = 0;
+        for(int right = 0; right < s.length(); right++){
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+            while(map.get(ch) > 2){
+                char lch = s.charAt(left);
+                map.put(lch, map.getOrDefault(lch,0) - 1);
+                if(map.get(lch) == 0){
+                    map.remove(lch);
+                }
+                left++;
+
+            }
+
+            maxlen = Math.max(maxlen, right - left + 1);
+        }
+        return maxlen;
+
+    }
+
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        System.out.println(maximumLengthSubstring(str));
+
+    }
+}
