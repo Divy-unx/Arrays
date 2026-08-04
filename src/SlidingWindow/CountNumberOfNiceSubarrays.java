8f313a60c51dc672d39b99a757c5ac8b7085a5ef
Divy-unx <divyanshum635@gmail.com>
2026-08-04 20:12:26 +0530

    Solved CountNumberOfNiceSubarrays DSA problem.

---
+++ src/SlidingWindow/CountNumberOfNiceSubarrays.java
@@ -0,0 +1,42 @@
+package SlidingWindow;
+
+import java.util.Scanner;
+
+public class CountNumberOfNiceSubarrays {
+    public static int atMost(int[] nums , int goal){
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+        int count = 0;
+
+        while(right <= nums.length - 1){
+            sum += nums[right] % 2;
+
+            while(sum > goal){
+                sum = sum - nums[left]%2;
+                left = left + 1;
+            }
+
+            count = count + (right - left + 1);
+            right++;
+        }
+        return count;
+    }
+
+    public static int NiceSubarrays(int[] nums, int goal){
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] nums = new int[num];
+
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        int goal = sc.nextInt();
+
+        System.out.println(NiceSubarrays(nums, goal));
+    }
+}
